a1495e6caf0c331767a4f1702ca12b1567bff94b
Luis Armando <armandon20@gmail.com>
2011-03-15 19:40:33 -0500

    Datos para que funcione SASPA

---
+++ doc/Modelo/initDBSaspa.sql
@@ -0,0 +1,19 @@
+--
+-- Data for Name: rol; Type: TABLE DATA; Schema: public; Owner: saspa
+--
+
+COPY rol (rol_identificador, rol_nombre, rol_url_menu, rol_url_inicio) FROM stdi
+n;
+adm     Administrador   administracion.php/inicio/menuAdministrador     administ
+racion.php/inicio/inicioAdministrador
+dip     Director de programa    direccion.php/inicio/menuDirector       direccio
+n.php/inicio/inicioDirector
+opr     OPDI Coordinardor de Area       coordinacion.php/inicio/menuCoordinador 
+coordinacion.php/inicio/inicioCoordinador
+opj     OPDI Jefatura   jefatura.php/inicio/menuJefatura        jefatura.php/ini
+cio/inicioJefatura
+dac     Daca    daca.php/inicio/menuDaca        daca.php/inicio/inicioDaca
+ana     OPDI Analisis   analisis.php/inicio/menuAnalisis        analisis.php/ini
+cio/inicioAnalisis
+\.
+
